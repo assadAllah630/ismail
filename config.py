9fb9c80fd8d0e4dfc8b032b2094fe56469f22399
assadAllah630 <163865907+assadAllah630@users.noreply.github.com>
2025-06-21 15:56:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev_secret')
     ADMIN_USERNAME = os.getenv('ADMIN_USERNAME', 'admin')
-    ADMIN_PASSWORD_HASH = os.getenv('ADMIN_PASSWORD_HASH', '')
+    ADMIN_PASSWORD_HASH = os.getenv('ADMIN_PASSWORD_HASH', '123')
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{os.getenv('DB_USER', 'root')}:{os.getenv('DB_PASSWORD', '')}"
         f"@{os.getenv('DB_HOST', 'localhost')}:{os.getenv('DB_PORT', '3306')}/{os.getenv('DB_NAME', 'ismail_furniture')}"
